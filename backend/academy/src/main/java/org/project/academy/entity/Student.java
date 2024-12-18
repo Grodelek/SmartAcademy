536dfa8183cc1712eeaf8067dbf2dfa099c6f29a
Grodelek <arczigrodel@wp.pl>
2024-12-18 23:02:41 +0100

    small changes

--- backend/academy/src/main/java/org/project/academy/entity/Student.java
+++ backend/academy/src/main/java/org/project/academy/entity/Student.java
@@ -3,24 +3,25 @@ package org.project.academy.entity;
 
 import jakarta.persistence.*;
 
+
+
 @Entity
 @Table(name="students")
 public class Student {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "email", nullable = false)
     private String email;
 
-    // Gettery i settery
-
-    public Long getId() {
-        return id;
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Student() {
     }
 
     public String getName() {
@@ -37,5 +38,13 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
