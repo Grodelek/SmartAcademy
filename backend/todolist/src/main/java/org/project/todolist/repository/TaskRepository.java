4717b204aef88e34fe7efb055af3884a74d5fa09
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-12-19 14:39:53 +0100

    Delete backend/todolist/src/main/java/org/project/todolist directory

--- backend/todolist/src/main/java/org/project/todolist/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package org.project.todolist.repository;
-
-import org.project.todolist.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
