0c7210cbe56743b87913a4074b80db88d1b889d1
Grodelek <arczigrodel@wp.pl>
2025-02-14 22:03:09 +0100

    delete

--- backend/academy/src/main/java/org/project/academy/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.project.academy.repository;
-
-import org.project.academy.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- backend/academy/src/main/java/org/project/academy/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.project.academy.repository;
-
-import org.project.academy.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
