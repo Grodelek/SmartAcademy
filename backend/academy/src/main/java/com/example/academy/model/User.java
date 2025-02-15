d311cfe321de0c8296cda41a957af09208fab10d
Grodelek <arczigrodel@wp.pl>
2025-02-15 20:33:21 +0100

    user custom creation

---
+++ backend/academy/src/main/java/com/example/academy/model/User.java
@@ -0,0 +1,50 @@
+package com.example.academy.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "roles")
+    private String roles;
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
