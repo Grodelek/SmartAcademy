7af1b4da2713c8fa0d6044571613d8b444f9c601
Grodelek <arczigrodel@wp.pl>
2025-03-12 21:13:59 +0100

    update put method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,32 @@
 import './App.css';
 import StudentList from "./components/Students/StudentList";
-import {BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router';
 import Home from "./pages/Home";
 import Login from "./components/Login/Login"
-import {AuthContext, AuthProvider} from "./components/Login/AuthContext";
-import {useContext, useEffect} from "react";
+import { AuthContext, AuthProvider } from "./components/Login/AuthContext";
+import { useContext, useEffect } from "react";
 import Register from "./components/Login/Register";
 import StudentRegisterForm from "./components/Students/StudentRegisterForm";
-
+import StudentUpdate from "./components/Students/StudentUpdate";
 function App() {
-    const { isAuthenticated, login, logout } = useContext(AuthContext);
+  const { isAuthenticated, login, logout } = useContext(AuthContext);
 
   return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/students" element={<StudentList/>}/>
-              <Route path="/students/add" element={<StudentRegisterForm/>}/>
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/register" element={<Register/>}/>
-          </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/students" element={<StudentList />} />
+        <Route path="/students/add" element={<StudentRegisterForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/students/update/:id" element={<StudentUpdate />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default () => (
-    <AuthProvider>
-        <App />
-    </AuthProvider>
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 );
